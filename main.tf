21871b95b4e8268f49c760a6ff985447c8248376
MirzaHasanBaig <nadeemb305@gmail.com>
2024-02-04 15:16:48 +0500

    cloudflare

---
+++ main.tf
@@ -0,0 +1,23 @@
+# Configure the Cloudflare provider using the required_providers stanza
+# required with Terraform 0.13 and beyond. You may optionally use version
+# directive to prevent breaking changes occurring unannounced.
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.token
+}
+
+resource "cloudflare_record" "terraform-add" {
+  zone_id = var.zone_id
+  name    = "terraform"
+  value   = "192.0.2.1"
+  type    = "A"
+  ttl     = 3600
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "zone_id" {
+  type    = string
+  default = "58585b65f5beab7cf2efbed62cfc7a95"
+}
+
+variable "token" {
+  type    = string
+  default = "test"
+}
+
